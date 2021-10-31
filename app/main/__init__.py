ec83357426d2b5eed32d452a5bcd28cbe34af5c5
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-31 15:22:06 +0300

    debugging

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,23 +1,18 @@
-from flask import Blueprint, Flask
+from flask import Blueprint
 main = Blueprint('main',__name__)
-from flask_bootstrap import Bootstrap
-from config import configOptions
-from . import views,error
-
-
-bootstrap = Bootstrap()
+from . import views
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(configOptions[config_name])
+    app.config.from_object(config_options[config_name])
 
    # Initializing flask extensions
     bootstrap.init_app(app)
 
     # Registering the blueprint
-    from main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_API_ARTICLE = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')          
+    NEWS_API_KEY = 'e090655fdf3c4a649f08c2eca7a1f2cc'
 class ProdConfig(Config):
     pass
 
